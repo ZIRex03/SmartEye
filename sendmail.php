7cef8debef6a228743cd48a56029e1a003aada00
ZIRex03 <116503458+ZIRex03@users.noreply.github.com>
2022-10-30 15:44:33 +0300

    Add files via upload

--- sendmail.php
+++ sendmail.php
@@ -7,8 +7,6 @@ use PHPMailer\PHPMailer\Exception;
 require './phpmailer/src/Exception.php';
 require './phpmailer/src/PHPMailer.php';
 
-$mail->SMTPDebug = 1;
-
 $mail = new PHPMailer(true);
 $mail->CharSet = 'UTF-8';
 $mail->setLanguage('ru', 'phpmailer/language/');
