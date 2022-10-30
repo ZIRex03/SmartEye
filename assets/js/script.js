a66ecf723fd0fd8783c57bb8bd4abdb072970a2b
ZIRex03 <116503458+ZIRex03@users.noreply.github.com>
2022-10-30 23:02:57 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if (error === 0) {
             form.classList.add('_sending');
-            let response = await fetch("sendmail.php", {
+            let response = await fetch('sendmail.php', {
                 method: 'POST',
                 body: formData
             });
